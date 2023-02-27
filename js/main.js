f13b56ed44c6b9fb56dee9d62ecb00800be08237
Denis Ryazanov <nppteploautomat2000@gmail.com>
2023-02-27 18:29:16 +0100

    REV_05_00

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
+$(document).ready(function(){
+    $('.slider__line').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        arrows: false,
+        responsive: [
+            {
+            breakpoint: 1860,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4
+            }
+            },
+            {
+            breakpoint: 1440,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+            },
+            {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+            },
+            {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            },
+        ]
+    });
+});
 (function () {
     const header = document.querySelector('.header');
     window.onscroll = () => {
